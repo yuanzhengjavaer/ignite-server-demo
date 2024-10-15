1ee0da24b286268764a8240ff304b0dc1f3807ab
yuanzheng <306423595@qq.com>
2024-10-15 20:17:44 +0900

    2th yuan

--- src/main/java/com/nomura/IgniteBaseServer.java
+++ src/main/java/com/nomura/IgniteBaseServer.java
@@ -1,10 +1,13 @@
 package com.nomura;
 
+import com.nomura.model.po.Maintenance;
 import com.nomura.model.po.Vehicle;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteTransactions;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.query.FieldsQueryCursor;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
@@ -12,6 +15,7 @@ import org.apache.ignite.transactions.Transaction;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 public class IgniteBaseServer {
@@ -21,6 +25,7 @@ public class IgniteBaseServer {
 //        props.setProperty("DIGNITE_SKIP_CONFIGURATION_CONSISTENCY_CHECK","true");
 //        System.setProperties();
         Ignite ignite = Ignition.start("ignite-config.xml");
+
 //        IgniteCache<Object, Object> vehicleCache = ignite.getOrCreateCache("vehicles");
 //        IgniteTransactions transactions = ignite.transactions();
 //        transactions.txStart();
@@ -33,6 +38,29 @@ public class IgniteBaseServer {
 //            System.out.println("拿不到同一个当前事务处理器");
 //        }
 //        tx1.rollback();
+//        tx1.close();
+
+        IgniteCache<Integer, Maintenance> maintenanceCache = ignite.cache("maintenance");
+        maintenanceCache.loadCache(null);
+        String sql = "INSERT INTO maintenance (id, vehicleId, date) VALUES (?, ?, ?)";
+        SqlFieldsQuery query = new SqlFieldsQuery(sql);
+        query.setArgs(1, 1, new Date());
+        FieldsQueryCursor<List<?>> res = maintenanceCache.query(query);
+        System.out.println(res.getAll());
+//        System.out.println("maintenanceCache = " + maintenanceCache);
+//        Object o = maintenanceCache.get(1);
+//        System.out.println("o = " + o);
+//        IgniteTransactions transactions = ignite.transactions();
+//        transactions.txStart();
+//        maintenanceCache.put(1, new Maintenance(null,1,new Date()));
+//        maintenanceCache.put(2, new Maintenance(null,2,new Date()));
+//        maintenanceCache.put(3, new Maintenance(null,3,new Date()));
+//        Transaction tx1 = transactions.tx();
+//        Transaction tx2 = transactions.tx();
+//        if (tx1 != tx2) {
+//            System.out.println("拿不到同一个当前事务处理器");
+//        }
+//        tx1.commit();
 //        tx1.close();
     }
 }
